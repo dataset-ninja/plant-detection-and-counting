653d7dce3b9e3aa957f5ebe031dcdb0fb88c1148
Arsenii Vladimirov <78355358+grokhi@users.noreply.github.com>
2023-11-07 15:36:17 +0600

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -73,9 +73,10 @@ AUTHORS: Optional[List[str]] = [
     "Benoit de Solan",
     "Frédéric Baret",
 ]
+AUTHORS_CONTACTS: Optional[List[str]] = ["etienne.david@inrae.fr"]
 
 ORGANIZATION_NAME: Optional[Union[str, List[str]]] = [
-    "INRAe, Arvalis, ITB and Hiphen (all - France)",
+    "INRAe, France", "Arvalis, France",  "ITB, France", "Hiphen, France",
 ]
 ORGANIZATION_URL: Optional[Union[str, List[str]]] = [
     "https://www.inrae.fr/en",
@@ -133,6 +134,7 @@ def get_settings():
     settings["repository"] = REPOSITORY
     settings["citation_url"] = CITATION_URL
     settings["authors"] = AUTHORS
+    settings["authors_contacts"] = AUTHORS_CONTACTS
     settings["organization_name"] = ORGANIZATION_NAME
     settings["organization_url"] = ORGANIZATION_URL
     settings["slytagsplit"] = SLYTAGSPLIT
